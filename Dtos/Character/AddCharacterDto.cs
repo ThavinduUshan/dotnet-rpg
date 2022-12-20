7e68d0b3b8f858ad6c7bba80674fb887a2b5a025
ThavinduUshan <thavinduushan@gmail.com>
2022-12-20 10:59:51 +0530

    Updated the Character Model

--- Dtos/Character/AddCharacterDto.cs
+++ Dtos/Character/AddCharacterDto.cs
@@ -7,6 +7,8 @@ namespace dotnet_rpg.Dtos.Character
 
         public string? Name { get; set; }
 
+        public int HitPoints { get; set; }
+
         public int Strength { get; set; }
         public int Defense { get; set; }
         public int Intelligence { get; set; }
--- models/Character.cs
+++ models/Character.cs
@@ -9,6 +9,8 @@ namespace dotnet_rpg.models
 
         public string? Name { get; set; }
 
+        public int HitPoints { get; set; }
+
         public int Strength { get; set; }
         public int Defense { get; set; }
         public int Intelligence { get; set; }
